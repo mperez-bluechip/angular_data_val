1ca8ecc9f9bbc90947b5ce05b6e559bb448ba64e
Manny Perez <threedyou@gmail.com>
2017-04-02 15:02:42 -0500

    Modified order-sheet.components.ts to include buildForm method with customer name   size, type of bread, type of Sandwich, other order requests, meat types, cheese types, and veggies

--- form_model/app/counter/order-sheet/order-sheet.component.ts
+++ form_model/app/counter/order-sheet/order-sheet.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, FormControl, FormArray } from '@angular/forms';
 import {DebugPanelComponent} from '../debug-panel/debug-panel.component';
 
 @Component({
@@ -11,13 +11,43 @@ import {DebugPanelComponent} from '../debug-panel/debug-panel.component';
 })
 export class OrderSheetComponent {
   orderSheetForm: FormGroup;
+  weirdRequestsControls: FormArray;
 
   constructor( private formBuilder: FormBuilder) {
     this.buildForm();
   }
   buildForm (){
     this.orderSheetForm = this.formBuilder.group({
-      
-    }); 
+      customerName: this.formBuilder.control(null),
+      size: this.formBuilder.control(null),
+      bread: this.formBuilder.control(null),
+      specialtySandwich: this.formBuilder.control(null),
+      weirdRequests: this.formBuilder.array([
+        this.formBuilder.control(null)
+      ]),
+      otherNotes: this.formBuilder.control(null),
+      meats: this.formBuilder.group({
+        meatHam: this.formBuilder.control(null),
+        meatTurkey: this.formBuilder.control(null),
+        meatRoastBeef: this.formBuilder.control(null)
+      }),
+      cheeses: this.formBuilder.group({
+        cheeseProvolone: this.formBuilder.control(null),
+        cheeseCheddar: this.formBuilder.control(null),
+        cheeseSwiss: this.formBuilder.control(null)
+      }),
+      veggiesAndSuch: this.formBuilder.group({
+        veggieLettuce: this.formBuilder.control(null),
+        veggieTomato: this.formBuilder.control(null),
+        veggieMustard: this.formBuilder.control(null)
+      })  
+    });
+    this.weirdRequestsControls = this.orderSheetForm.get('weirdRequests') as FormArray;
+  }
+  onAddWeirdRequest(){
+    this.weirdRequestsControls.push(this.formBuilder.control(null));
+  }
+  onRemoveWeirdRequest(index){
+    this.weirdRequestsControls.removeAt(index);
   }
 }
